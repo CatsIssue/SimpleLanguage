791427c9857d7f738e64cfb4ece7d0f1abac288e
Cat_Issue <43684350+CatsIssue@users.noreply.github.com>
2019-08-02 08:43:53 +0300

    Add files via upload

---
+++ A_Simple_kaleidoscope_Language/A_Simple_kaleidoscope_Language/BinaryExpressionParsing.h
@@ -0,0 +1,6 @@
+#pragma once
+#include"Parser.h"
+//---------------------------Binary Expressions Parsing----------------------------------------
+
+// use Operator-precedence parsing 
+// BinopPrecedence - this holds precedence for each binary operator that is defined 
---
+++ A_Simple_kaleidoscope_Language/A_Simple_kaleidoscope_Language/Language_implementation.cpp
@@ -0,0 +1,34 @@
+
+#include <algorithm>
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+#include "TopLevelParsing.h"
+
+
+
+//===----------------------------------------------------------------------===//
+// Main driver code.
+//===----------------------------------------------------------------------===//
+
+int main() {
+	// Install standard binary operators.
+	// 1 is lowest precedence.
+	BinopPrecedence['<'] = 10;
+	BinopPrecedence['+'] = 20;
+	BinopPrecedence['-'] = 20;
+	BinopPrecedence['*'] = 40; // highest.
+
+	// Prime the first token.
+	fprintf(stderr, "ready> ");
+	getNextToken();
+
+	// Run the main "interpreter loop" now.
+	MainLoop();
+
+	return 0;
+}
